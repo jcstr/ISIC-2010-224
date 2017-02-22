c1a2887e7d01ee684c152987800ea3407527e319
Jesús Castro <51v4n@openmailbox.org>
2017-02-22 15:13:08 -0600

    the note for today
    just concepts
    
    [ticket: ds]

---
+++ nts/recursividad.cpp
@@ -0,0 +1,44 @@
+/*
+	22/02/2017
+	Recursividad
+	- caracteritica que tiene un sistema de estar estrucralmente a reglas
+	recurrentes o recursivas
+	
+	Geometria fractal
+	- sistemas fractales
+
+	Recurrencia
+	- regla matematica
+	consiste de 2 partes
+	- base: se presenta la solucion explicita a las instancias mas simples
+			objeto original, el mas simple de toda la serie
+
+	- induccion:
+	regla que indica el procedimiento a seguir
+
+		- regla de exlcusion: objeto que no sea generado inductivamente
+		no pertenece a la serie
+
+	*programacion estructurada: forma regular **
+
+	--
+	numeros naturales
+
+		sea n, conjunto de numeros naturales
+
+	base: 0 e N // 0 numero natural
+	induccion: si x e N entonces x + 1 e N // si x es un numero natural
+	x + 1 tambien es un numero natural
+*/
+
+#include <cstdio>
+
+/*
+	
+*/
+
+
+int main()
+{
+	
+}
