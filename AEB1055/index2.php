bb6fdb9275bf7328d0c3a45870b5d8adb7d564eb
Jesús Castro <x51v4n@gmail.com>
2018-05-25 02:13:30 -0500

    php-mysql: practice finishied
    This simple proyect let us save students registrations to a mysql
    database and display the data to an html table.
    
    Signed-off-by: Jesús Castro <x51v4n@gmail.com>

---
+++ AEB1055/index2.php
@@ -0,0 +1,131 @@
+<?php
+$db_host = 'localhost'; // Server Name
+$db_user = 'root'; // Username
+$db_pass = 'root'; // Password
+$db_name = 'scotchbox'; // Database Name
+
+$conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+if (!$conn) {
+	die ('Failed to connect to MySQL: ' . mysqli_connect_error());	
+}
+
+$sql = 'SELECT * 
+		FROM persons';
+		
+$query = mysqli_query($conn, $sql);
+
+if (!$query) {
+	die ('SQL Error: ' . mysqli_error($conn));
+}
+?>
+<html>
+<head>
+	<title>Resgitros</title>
+	<style type="text/css">
+		body {
+			font-size: 15px;
+			color: #343d44;
+			font-family: "segoe-ui", "open-sans", tahoma, arial;
+			padding: 0;
+			margin: 0;
+		}
+		table {
+			margin: auto;
+			font-family: "Lucida Sans Unicode", "Lucida Grande", "Segoe Ui";
+			font-size: 12px;
+		}
+
+		h1 {
+			margin: 25px auto 0;
+			text-align: center;
+			text-transform: uppercase;
+			font-size: 17px;
+		}
+
+		table td {
+			transition: all .5s;
+		}
+		
+		/* Table */
+		.data-table {
+			border-collapse: collapse;
+			font-size: 14px;
+			min-width: 537px;
+		}
+
+		.data-table th, 
+		.data-table td {
+			border: 1px solid #e1edff;
+			padding: 7px 17px;
+		}
+		.data-table caption {
+			margin: 7px;
+		}
+
+		/* Table Header */
+		.data-table thead th {
+			background-color: #508abb;
+			color: #FFFFFF;
+			border-color: #6ea1cc !important;
+			text-transform: uppercase;
+		}
+
+		/* Table Body */
+		.data-table tbody td {
+			color: #353535;
+		}
+		.data-table tbody td:first-child,
+		.data-table tbody td:nth-child(4),
+		.data-table tbody td:last-child {
+			text-align: right;
+		}
+
+		.data-table tbody tr:nth-child(odd) td {
+			background-color: #f4fbff;
+		}
+		.data-table tbody tr:hover td {
+			background-color: #ffffa2;
+			border-color: #ffff0f;
+		}
+
+		/* Table Footer */
+		.data-table tfoot th {
+			background-color: #e5f5ff;
+			text-align: right;
+		}
+		.data-table tfoot th:first-child {
+			text-align: left;
+		}
+		.data-table tbody td:empty
+		{
+			background-color: #ffcccc;
+		}
+	</style>
+</head>
+<body>
+	<h1>Registros</h1>
+	<table class="data-table">
+		<caption class="title">Estudiantes del departamento de ingenieria de software</caption>
+		<thead>
+			<tr>
+				<th>NOMBRE</th>
+				<th>APELLIDO</th>
+				<th>CORREO</th>
+			</tr>
+		</thead>
+		<tbody>
+		<?php
+		$no = 1;
+		$total = 0;
+		while ($row = mysqli_fetch_array($query)) {
+		echo '<tr>
+				<td>'.$row['first_name'].'</td>
+				<td>'.$row['last_name'].'</td>
+				<td>'.$row['email'].'</td>
+			  </tr>';	
+		}
+		?>
+		</tbody>
+		</table>
+</body>
+</html>
